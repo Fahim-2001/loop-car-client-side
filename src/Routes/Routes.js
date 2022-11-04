34ee6f9cacb7d02d26641529a4c877ed1d2e441e
unknown <mdyasirofficial2001@gmail.com>
2022-11-04 12:33:40 +0600

    First Commit

---
+++ src/Routes/Routes.js
@@ -0,0 +1,22 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../Layout/Main");
+const { default: Home } = require("../Pages/Home/Home/Home");
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
